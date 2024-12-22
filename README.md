e0c127992f9ac822bbe15a956f258d9ccf71544f
aldona-c <aldona.cepliejute@gmail.com>
2024-12-22 12:09:57 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Run `pnpm i` to install the project's packages.
-
-Followed by `pnpm dev` to start the development server.
--- app/ui/page-wrapper/page-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page-wrapper.module.css";
-
-export default function PageWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className={styles.pageWrapper}>{children}</div>;
-}
